57c963c729e161b9c3809ff5f445110289bf29f2
Dragu Costel <dragu.costel@yahoo.com>
2017-11-20 21:59:09 +0200

    update server

--- src/restfull/main.go
+++ src/restfull/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	//"log"
+	"log"
 	"net/http"
 	"restfull/server"
+	"github.com/gorilla/mux"
 )
+
+const (
+	Port = ":8080"
+)
+
 /**
  * Initialization of the program
  */
-//func init() {
-//	log.SetPrefix("TRACE: ")
-//	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
-//}
+func init() {
+	log.SetPrefix("TRACE: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+}
 
 func main() {
-	//log.Println("Server has start")
-	http.HandleFunc("/",webserver.ServeDynamic )
-	http.ListenAndServe(webserver.Port,nil)
+	log.Println("Server has start")
+	routes := mux.NewRouter()
+	routes.HandleFunc("/api/{guid:[0-9a-zA\\-]+}",webserver.ServeDynamic ).Methods("GET")
+	routes.HandleFunc("/pages/{id:[0-9]+}", webserver.PageHandler).Methods("GET")
+	routes.HandleFunc("/{pageId:[0-9a-zA\\-]+}", webserver.ServePage)
+	http.Handle("/", routes)
+	http.ListenAndServe(Port,nil)
 }
--- src/restfull/server/webserver.go
+++ src/restfull/server/webserver.go
@@ -3,13 +3,31 @@ package webserver
 import (
 	"net/http"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 )
 
-const (
-	Port = ":8080"
-)
 
 func ServeDynamic(w http.ResponseWriter, r *http.Request) {
-	response := "The time is now "
+	vars := mux.Vars(r)
+	pageGUID := vars["guid"]
+	response := "ServeDynamic " + pageGUID
+	log.Println("Server Dynamic page" + pageGUID)
+	fmt.Fprintln(w,response)
+}
+
+func PageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	response := "PageHandler " + pageID
+	log.Println("Page Handler page" + pageID)
+	fmt.Fprintln(w,response)
+}
+
+func ServePage(w http.ResponseWriter, r *http.Request)  {
+	vars := mux.Vars(r);
+    pageID := vars["pageId"]
+	response := "ServePage " + pageID
+	log.Println("Page Handler page" + pageID)
 	fmt.Fprintln(w,response)
 }
